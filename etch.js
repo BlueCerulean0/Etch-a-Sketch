134668e2f51ffcc704efca20c660d910c7dcc35a
Roy <126029801+BlueCerulean0@users.noreply.github.com>
2024-10-27 16:02:12 +0000

    Works

--- etch.js
+++ etch.js
@@ -2,12 +2,20 @@ const container = document.getElementById('container');
 const start = document.getElementById('start');
 const reset = document.getElementById('reset');
 const num = document.getElementById('num');
+const input = document.getElementById('grid-size');
+const gridValue1 = document.querySelector('.value1');
+const gridValue2 = document.querySelector('.value2');
+const colorPicker = document.getElementById('colorPicker')
 
-function createGrid(size='16', color='black') {
+let currentColor = '#000000';
+
+function createGrid(size= '16', color= currentColor) {
+
+    container.innerHTML = ''
 
     container.style.display = 'flex';
     container.style.flexWrap = 'wrap';
-    //container.style.width = '560px';
+    //container.style.width = '';
 
     const totalDiv = size * size;
     const divSize = 500 / size;
@@ -17,7 +25,7 @@ function createGrid(size='16', color='black') {
         const div = document.createElement('div');
         div.style.width = `${divSize}px`;
         div.style.height = `${divSize}px`;
-        //div.style.border = '1px solid #ccc';
+        //div.style.border = '1px solid';
         div.style.boxSizing = 'border-box';
 
 
@@ -31,6 +39,30 @@ function createGrid(size='16', color='black') {
     }
 }
 
+colorPicker.addEventListener('input', (e) => {
 
-start.onclick = function () {createGrid()}
-reset.onclick = function resetGrid() {container.innerHTML = ''};
+    currentColor = e.target.value
+    createGrid(input.value, currentColor)
+    //colorPicker.style.backgroundColor = currentColor
+})
+
+input.addEventListener('input', (e) => {
+    const divSize = e.target.value;
+    gridValue1.textContent = divSize;
+    gridValue2.textContent = divSize;
+    createGrid(divSize);
+})
+
+input.value = 16;
+gridValue1.textContent = 16;
+gridValue2.textContent = 16;
+createGrid();
+
+reset.onclick = function resetGrid() {
+    container.innerHTML = ''
+    currentColor = black
+    input.value = 16 ;
+    gridValue1.textContent = 16;
+    gridValue2.textContent = 16;
+    createGrid();
+};
